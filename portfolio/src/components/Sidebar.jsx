465c06013c7a36ea08b96314c183f6a1fd911aeb
Daniel Custodio <play.daniel@hotmail.com>
2023-04-08 14:55:59 -0300

    feat: Social networks section created

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Avatar from '../img/perfil.jpeg';
 
 import '../styles/components/sidebar.sass';
+import SocialNetworks from './SocialNetworks';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Daniel Custódio" />
       <p className="title">Web Developer</p>
-      <p>redes sociais</p>
+      <SocialNetworks />
       <p>informações de contato</p>
       <a href="" className="btn">
         Download Currículo
